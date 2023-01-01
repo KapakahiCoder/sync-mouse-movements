8a2decb30aabb5ce694dc6941e79567dffebb185
Kapakahi <rubbishman@gmail.com>
2023-01-02 07:04:45 +0900

    set up firebase config file

---
+++ public/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBZyGdP0FeJyXCswNkkkT3dIQL-ThENvYE",
+  authDomain: "sync-mouse-movements-1fb74.firebaseapp.com",
+  projectId: "sync-mouse-movements-1fb74",
+  storageBucket: "sync-mouse-movements-1fb74.appspot.com",
+  messagingSenderId: "25941787707",
+  appId: "1:25941787707:web:f1dd443ab2eba5d021081e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
