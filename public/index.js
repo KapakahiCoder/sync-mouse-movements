4b2a3268359e6ad52550c99270731a48849b7129
Kapakahi <rubbishman@gmail.com>
2023-01-03 04:25:12 +0900

    get mouse positions from the DB and create a virtual mouse pointer

--- public/index.js
+++ public/index.js
@@ -16,6 +16,26 @@ document.addEventListener("mousemove", (event) => {
   let xyCoords = { x, y };
   console.log(xyCoords);
 
+  // Record mouse movements in the DB
   const newCordsRef = push(coordsListRef);
   set(newCordsRef, { coords: xyCoords });
+});
+
+let coordsArray = [];
+let x;
+let y;
+let coordsObj = {};
+onValue(coordsListRef, (snapshot) => {
+  snapshot.forEach((child) => {
+    x = child.val().coords.x;
+    y = child.val().coords.y;
+    coordsObj = { x, y };
+    coordsArray.push(coordsObj);
+  });
+
+  let newX = coordsArray[coordsArray.length - 1].x;
+  let newY = coordsArray[coordsArray.length - 1].y;
+  console.log(newX, newY, "xyxyxyxyxyx");
+  pointer.style.left = newX + "px";
+  pointer.style.top = newY + "px";
 });
